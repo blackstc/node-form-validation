aee895b767cb882923cf726fe60b595c70049d43
Blackstock <charlieblackstock@gmail.com>
2015-08-24 11:42:52 -0600

    added validation and ability to add people/puppies

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,5 +1,6 @@
 // add scripts
 
-$(document).on('ready', function() {
-  console.log('sanity check!');
-});
+// $(document).on('ready', function() {
+//   console.log('sanity check!');
+//   console.log(newPuppy);
+// });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,81 @@
 var express = require('express');
 var router = express.Router();
+var puppies = [];
+var people = [];
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post("/puppies/new", function(req, res, next) {
-  res.render("newPuppy");
+router.get('/puppies/new', function(req, res, next) {
+  res.render('newPuppy');
 });
+
+router.get('/people/new', function(req, res, next) {
+  res.render('newPeople');
+});
+
+router.post("/puppies", function(req, res, next) {
+  var name = req.body.name;
+  var id = req.body.id;
+  var puppy = new Puppy(name, id);
+  var errors = validationCheck(name, id);
+  console.log(errors);
+  if (errors.length > 0) {
+    res.render("newPuppy", {errors: errors});
+  } else {
+    puppies.push(puppy);
+    res.render("puppies", {
+      puppies: puppies,
+      success: "The puppy was saved successfully"
+    });
+  }
+});
+
+router.post("/people", function(req, res, next) {
+  var name = req.body.name;
+  var id = req.body.id;
+  var person = new Person(name, id);
+  var errors = validationCheck(name, id);
+  if (errors.length > 0) {
+    res.render("newPeople", {errors: errors});
+  } else {
+    people.push(person);
+    res.render("people", {
+      people: people,
+      success: "The person was saved successfully"
+    });
+  }
+});
+
+var Puppy = function(name, id) {
+  this.name = name;
+  this.id = id;
+};
+
+var Person = function(name, id) {
+  this.name = name;
+  this.id = id;
+};
+
+function validationCheck(name, id) {
+
+  var errorArray = [];
+  var nameTrimmed = name.trim();
+  var idTrimmed = id.trim();
+
+  if(nameTrimmed === '') {
+    errorArray.push("Name cannot be blank.");
+  }
+
+  if(idTrimmed === '') {
+    errorArray.push('Id cannot be blank.');
+  } else if (idTrimmed.length < 3) {
+    errorArray.push('A Id must be at least 3 characters long.');
+  }
+
+  return errorArray;
+
+}
 
 module.exports = router;
